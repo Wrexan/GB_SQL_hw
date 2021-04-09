1eeef1e9c3c365981d3dd4e4c8504be19d0d9933
Wrexan <76569072+Wrexan@users.noreply.github.com>
2021-04-09 18:04:29 +0300

    Add files via upload

---
+++ lesson_05/hw5_operands_filtering_4.sql
@@ -0,0 +1,32 @@
+/* «Операторы, фильтрация, сортировка и ограничение». Для задания 4. */
+/* «Агрегация данных». Для заданий 1, 2. */
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Покупатели';
+
+INSERT INTO
+  users (name, birthday_at)
+VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '2006-08-29');
+  
+ 
+-- (по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае. Месяцы заданы в виде списка английских названий (may, august)
+
+
+SELECT name,
+CASE
+	WHEN month(birthday_at) = 5 THEN 'may'
+	WHEN month(birthday_at) = 8 THEN 'august'
+END AS birth_month FROM users WHERE month(birthday_at) IN (5, 8);
+-- Не нашел, как сократить количество вызовов ф-ции month().
+-- Декларация переменных не работает.
