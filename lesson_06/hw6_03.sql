643b3ca7cbd274f827a57ce2f56d99722426c6e1
Wrexan <76569072+Wrexan@users.noreply.github.com>
2021-04-14 16:35:36 +0300

    add 'fm' if equal

--- lesson_06/hw6_03.sql
+++ lesson_06/hw6_03.sql
@@ -4,12 +4,7 @@
 -- SELECT count(like_type) FROM posts_likes WHERE (SELECT gender FROM profiles WHERE profiles.user_id = posts_likes.user_id LIMIT 2) = 'm' AND like_type
 
 -- SELECT count(like_type) FROM posts_likes WHERE (SELECT gender FROM profiles WHERE profiles.user_id = posts_likes.user_id LIMIT 2) = 'f' AND like_type
-		
-SELECT IF(
-(SELECT count(like_type) FROM posts_likes WHERE like_type AND (SELECT gender FROM profiles WHERE profiles.user_id = posts_likes.user_id) = 'f') >
-(SELECT count(like_type) FROM posts_likes WHERE like_type AND (SELECT gender FROM profiles WHERE profiles.user_id = posts_likes.user_id) = 'm'),
-'f', 'm') most_of_all_likes
-	
 
-
-
+SET @f := (SELECT count(like_type) FROM posts_likes WHERE like_type AND (SELECT gender FROM profiles WHERE profiles.user_id = posts_likes.user_id) = 'f');
+SET @m := (SELECT count(like_type) FROM posts_likes WHERE like_type AND (SELECT gender FROM profiles WHERE profiles.user_id = posts_likes.user_id) = 'm');
+SELECT IF(@f = @m,'fm', IF(@f > @m, 'f', 'm')) most_likers;
