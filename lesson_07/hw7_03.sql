172ac503bf2bd40902c281f292f0364f5796c02e
Wrexan <76569072+Wrexan@users.noreply.github.com>
2021-04-18 23:57:59 +0300

    add solution

--- lesson_07/hw7_03.sql
+++ lesson_07/hw7_03.sql
@@ -33,12 +33,15 @@ INSERT INTO cities VALUES
   ('kazan', 'Казань'),
   ('omsk', 'Омск');
 
-
+-- solution without join
 SELECT 
 	(SELECT name FROM cities WHERE flights.fly_from = cities.label) point_A,
 	' ==> ',
 	(SELECT name FROM cities WHERE flights.fly_to = cities.label) point_B
 FROM flights;
 
-
+-- solution with join
+SELECT point_a.name AS point_a, ' ==> ', point_b.name AS point_b FROM flights f
+LEFT JOIN cities as point_a on point_a.label = f.fly_from
+LEFT JOIN cities as point_b on point_b.label = f.fly_to
 
