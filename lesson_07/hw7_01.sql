40aaabd9549260e5c1a2f9d85165506625cc06cf
Wrexan <76569072+Wrexan@users.noreply.github.com>
2021-04-18 22:03:28 +0300

    add another solution

--- lesson_07/hw7_01.sql
+++ lesson_07/hw7_01.sql
@@ -1,7 +1,6 @@
 -- 1. Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
 
-
+-- Решение вложенным запросом
 SELECT name FROM users WHERE users.id in (SELECT DISTINCT user_id FROM orders)
-
-
-
+-- Решение с помощью JOIN
+SELECT DISTINCT name FROM users JOIN orders ON (users.id = user_id)
