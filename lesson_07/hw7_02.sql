794995f577e443df28ad38adc58c6cf317f79cf7
Wrexan <76569072+Wrexan@users.noreply.github.com>
2021-04-18 22:48:27 +0300

    additional solution

--- lesson_07/hw7_02.sql
+++ lesson_07/hw7_02.sql
@@ -1,7 +1,6 @@
 -- 2. Выведите список товаров products и разделов catalogs, который соответствует товару.
 
+-- Solution without join
 SELECT name, ifnull((SELECT name FROM catalogs WHERE catalogs.id = products.catalog_id), 'Нет категории') category FROM products
-
-
-
-
+-- Solution with join
+SELECT products.name, ifnull(catalogs.name, 'Нет категории') from products LEFT JOIN catalogs ON products.catalog_id = catalogs.id
